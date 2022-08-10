fc7f513462bce53bd406120be9a3a1d6642acd59
Dan Cab <dancab489@gmail.com>
2022-08-09 22:00:39 -0700

    console log

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 no = false;
+//boolean
+
+/*
+    here is one line
+    here is another line
+    and another one
+*/
+const humanTime = {name:"Daniel Cabrera", favFood:"ribs?"};
+console.log(humanTime);
+
